275d6384eea4ce3018e3d28056520392c9866487
Roman Malyi <madphysicsm@gmail.com>
2020-03-20 21:25:08 +0200

    Empty projects added

---
+++ src/Golem.Api/Controllers/AnalyticsController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Golem.Api.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class AnalyticsController : ControllerBase
+    {
+        [HttpGet("analytics")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
