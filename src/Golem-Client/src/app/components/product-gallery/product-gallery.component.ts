cf6f238bf23d44bccc38feeced0283e25fedbd2d
Roman Malyi <madphysicsm@gmail.com>
2020-03-26 22:07:37 +0200

    Added home page design for desktop

--- src/Golem-Client/src/app/components/product-gallery/product-gallery.component.ts
+++ src/Golem-Client/src/app/components/product-gallery/product-gallery.component.ts
@@ -6,15 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-gallery.component.scss']
 })
 export class ProductGalleryComponent implements OnInit {
-  public products: string[] = [
+  public products  = [
     // tslint:disable-next-line: max-line-length
-    'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+    {text: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+    photo: '../../../assets/product_1.jpg'},
     // tslint:disable-next-line: max-line-length
-    'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.',
+    {text: 'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage.',
+    photo: '../../../assets/Product_2.jpg'},
     // tslint:disable-next-line: max-line-length
-    'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.',
+    {text: 'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. ',
+    photo: '../../../assets/Product_3.jpg'},
     // tslint:disable-next-line: max-line-length
-    'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.'
+    {text: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable.',
+    photo: '../../../assets/Product_4.jpg'}
   ];
 
   constructor() {}
