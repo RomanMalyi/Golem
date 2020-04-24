90b0e8145352ce984226eb7ce27f0ac640e8c96b
Roman Malyi <madphysicsm@gmail.com>
2020-04-24 19:50:03 +0300

    Added client search

--- src/Golem-Client/src/app/components/app/app.component.ts
+++ src/Golem-Client/src/app/components/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/models/project';
+import { ElasticsearchService } from 'src/app/services/elasticsearch.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Golem-Client';
+export class AppComponent implements OnInit {
+  constructor(private searchService: ElasticsearchService) {}
+
+  public products: Project[];
+
+  ngOnInit(): void {
+    this.getProjects('');
+  }
+
+  public onKeyUp(event: any) {
+    this.getProjects(event.target.value);
+  }
+
+  private getProjects(searchTerm: string) {
+    this.searchService.search(searchTerm).subscribe(
+      (res) => {
+        this.products = res;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
--- src/Golem-Client/src/app/components/product-gallery/product-gallery.component.ts
+++ src/Golem-Client/src/app/components/product-gallery/product-gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ElasticsearchService } from 'src/app/services/elasticsearch.service';
 import { Project } from 'src/app/models/project';
 
@@ -7,23 +7,6 @@ import { Project } from 'src/app/models/project';
   templateUrl: './product-gallery.component.html',
   styleUrls: ['./product-gallery.component.scss']
 })
-export class ProductGalleryComponent implements OnInit {
-  public products: Project[];
-
-  constructor(private searchService: ElasticsearchService) {}
-
-  ngOnInit(): void {
-    this.getProjects('');
-  }
-
-  private getProjects(searchTerm: string) {
-    this.searchService.search(searchTerm).subscribe(
-      res => {
-        this.products = res;
-      },
-      error => {
-        console.log(error);
-      }
-    );
-  }
+export class ProductGalleryComponent {
+  @Input() public products: Project[];
 }
