45d096a58bfc6faff9fb1d8cdc80644a19ff175d
Roman Malyi <madphysicsm@gmail.com>
2020-04-28 22:19:56 +0300

    Added features

--- src/Golem-Client/src/app/components/features/features.component.ts
+++ src/Golem-Client/src/app/components/features/features.component.ts
@@ -3,20 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-features',
   templateUrl: './features.component.html',
-  styleUrls: ['./features.component.scss']
+  styleUrls: ['./features.component.scss'],
 })
-export class FeaturesComponent implements OnInit {
-  public features: string[] = [
-    // tslint:disable-next-line: max-line-length
-    'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC.',
-    // tslint:disable-next-line: max-line-length
-    'Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur.',
-    // tslint:disable-next-line: max-line-length
-    'Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero.'
-  ];
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class FeaturesComponent {
+  constructor() {}
 }
