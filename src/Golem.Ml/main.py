61dd03e1e684a8a37d08dd7024689f330336e4c6
Roman Malyi <rmalyi@exoft.net>
2020-06-14 14:02:16 +0300

    Changed ml result to 4 words

--- src/Golem.Ml/main.py
+++ src/Golem.Ml/main.py
@@ -9,8 +9,11 @@ app = Flask(__name__)
 
 @app.route("/similar")
 def most_similar():
-    words = request.args.get('words').split()
-    result = model.most_similar(positive=words, topn=1)
-    return result[0][0];
+    words = request.args.get('words').lower().split()
+    similar = model.most_similar(positive=words, topn=4)
+    result = ""
+    for word in similar:
+        result += word[0] + " "
+    return result;
 
 app.run(host='0.0.0.0', port=80, debug=False)
